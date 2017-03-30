87804f2b70cc1fa9cb4e1c4d50c513a6f4097efb
sbrumley <sbrumley@wireless-town.com>
2017-03-30 15:39:58 -0400

    Added provisioner to vagrant up

--- vssh.sh
+++ vssh.sh
@@ -4,10 +4,10 @@ vagrant plugin install vagrant-vbguest
 
 if [ "${1}" == "cuckoo" ]; then
     echo "Set Up Cuckoo"
-    VAGRANT_VAGRANTFILE=Vagrantfile-cuckoo vagrant up
+    VAGRANT_VAGRANTFILE=Vagrantfile-cuckoo vagrant up --provider=virtualbox
 else
    echo "Web API Only"
-    vagrant up
+    vagrant up --provider=virtualbox
 fi
 
 vagrant ssh
