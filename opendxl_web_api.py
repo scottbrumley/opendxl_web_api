ec1d82f469449cdf6cf0637582316193b1d29b53
sbrumley <sbrumley@wireless-town.com>
2017-03-30 17:56:25 -0400

    Created TIE get route for SHA1 and one for MD5

--- opendxl_web_api.py
+++ opendxl_web_api.py
@@ -118,11 +118,7 @@ def tie():
     return "Path needs to be /service/action"
 ##### END TIE #####
 
-### TIE GET FILE REP
-@app.route('/tie/get/md5/<path:md5>/sha1/<path:sha1>')
-@app.route('/tie/get/md5/<path:md5>/', defaults={'sha1': ''})
 def getTieRep(md5,sha1):
-
     if not is_hex(md5):
         return "MD5 Value should be hex"
     if not is_hex(sha1) and not sha1 == "":
@@ -131,51 +127,72 @@ def getTieRep(md5,sha1):
     md5_hex = md5
     sha1_hex = sha1
 
+    with DxlClient(config) as client:
+        # Connect to the fabric
+        client.connect()
+        #
+        # Request and display reputation for Putty
+        #
+        response_dict = get_tie_file_reputation(client=client,
+                                                md5_hex= md5_hex,
+                                                sha1_hex=sha1_hex)
+    ### Dump Dictionary into JSON
+    myReturnVal = json.dumps(response_dict, sort_keys=True, indent=4, separators=(',', ': '))
+    return myReturnVal
 
-    ## Make sure an MD5 or SHA1 hash are given
-    if (md5_hex) or (sha1_hex):
-        with DxlClient(config) as client:
-            # Connect to the fabric
-            client.connect()
-            #
-            # Request and display reputation for Putty
-            #
-            response_dict = get_tie_file_reputation(client=client,
-                                                    md5_hex= md5_hex,
-                                                    sha1_hex=sha1_hex)
+def getFileProps(myReturnVal):
+    fileProps = json.loads(myReturnVal)
+    print fileProps['props']
+    propList = []
 
+    for provider in fileProps['reputations']:
+        propDict = {}
+        providerID = int(provider['providerId'])
+        trustLevel = int(provider['trustLevel'])
+
+        print "Create Date: " + str(provider['createDate'])
+        if (providerID in providerMap):
+            print "Provider: " + str(providerMap[providerID])
+            propDict['provider'] = str(providerMap[providerID])
+        else:
+            print "Provider: Not Available"
+            propDict['provider'] = "Not Available"
+
+        if (trustLevel in tiescoreMap):
+            print "Reputation: " + str(tiescoreMap[trustLevel])
+            propDict['reputation'] = str(tiescoreMap[trustLevel])
+        else:
+            print "Reputation: Not Available"
+            propDict['reputation'] = "Not Available"
+        print ""
+        propList.append(propDict)
+    return propList
+
+### TIE GET FILE REP
+#@app.route('/tie/get/<path:md5>/<path:sha1>')
+#@app.route('/tie/get/<path:md5>/', defaults={'sha1': ''})
+@app.route('/tie/get/md5/<path:md5>/')
+def getMD5Rep(md5):
+    if (md5):
         ### Filename You are Checking in TIE
         filename = ""
-        ### Dump Dictionary into JSON
-        myReturnVal = json.dumps(response_dict, sort_keys=True, indent=4, separators=(',', ': '))
+        myReturnVal = getTieRep(md5,"")
         ### Load JSON into fileProps Dictionary
-        fileProps = json.loads(myReturnVal)
+        propList = getFileProps(myReturnVal)
+        return render_template('reputation.html', md5=md5, filename=filename, propList=propList, myReturnVal=myReturnVal,action="getfile")
+    else:
+        myReturnVal = "Sorry Nobody Home"
+        return myReturnVal
 
-        print fileProps['props']
-        propList = []
-
-        for provider in fileProps['reputations']:
-            propDict = {}
-            providerID = int(provider['providerId'])
-            trustLevel = int(provider['trustLevel'])
-
-            print "Create Date: " + str(provider['createDate'])
-            if (providerID in providerMap):
-                print "Provider: " + str(providerMap[providerID])
-                propDict['provider'] = str(providerMap[providerID])
-            else:
-                print "Provider: Not Available"
-                propDict['provider'] = "Not Available"
-
-            if (trustLevel in tiescoreMap):
-                print "Reputation: " + str(tiescoreMap[trustLevel])
-                propDict['reputation'] = str(tiescoreMap[trustLevel])
-            else:
-                print "Reputation: Not Available"
-                propDict['reputation'] = "Not Available"
-            print ""
-            propList.append(propDict)
-        return render_template('reputation.html', md5=md5, sha1=sha1, filename=filename, propList=propList, myReturnVal=myReturnVal,action="getfile")
+@app.route('/tie/get/sha1/<path:sha1>/')
+def getSHA1Rep(sha1):
+    if (sha1):
+        ### Filename You are Checking in TIE
+        filename = ""
+        myReturnVal = getTieRep(sha1,"")
+        ### Load JSON into fileProps Dictionary
+        propList = getFileProps(myReturnVal)
+        return render_template('reputation.html', sha1=sha1, filename=filename, propList=propList, myReturnVal=myReturnVal,action="getfile")
     else:
         myReturnVal = "Sorry Nobody Home"
         return myReturnVal
