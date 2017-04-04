66d347c261f20725da3b1acfe924fd640edbf711
sbrumley <sbrumley@wireless-town.com>
2017-04-04 14:31:55 -0400

    Created Stop and Start.  Setup so that a failed test will stop flask.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,40 @@
 #!/usr/bin/env bash
 
-FLASK_APP="/vagrant/opendxl_web_api.py"
-/usr/local/bin/flask run --host=0.0.0.0&
-FLASK_PID="$!"
+FLASK_PID_FILE="/tmp/flask_pid"
 
-# do other stuff
-echo $FLASK_PID
-sleep 5
-/vagrant/tests/urltests.sh
+if [ -f ${FLASK_PID_FILE} ]; then
+    source ${FLASK_PID_FILE}
+fi
 
-echo "Killing Flask"
-KILLRET=$(sudo kill -SIGTERM $FLASK_PID 2>&1)
+function start {
+    echo "Running Flask ..."
+    FLASK_APP="/vagrant/opendxl_web_api.py"
+    /usr/local/bin/flask run --host=0.0.0.0&
+    FLASK_PID="$!"
+    echo "FLASK_PID=${FLASK_PID}" > "${FLASK_PID_FILE}"
+    sleep 5
+}
+function stop {
+    echo "Stopping Flask ..."
+    KILLRET=$(sudo kill -SIGTERM $FLASK_PID 2>&1)
+    rm -rf ${FLASK_PID_FILE}
+}
 
+function test {
+    /vagrant/tests/urltests.sh
+    if [[ $? == 1 ]]; then
+        stop
+    fi
+}
+
+if [[ ${1} == "start" ]]; then
+    start
+    # Testing
+    test
+    echo "Flask Started"
+fi
+
+if [[ ${1} == "stop" ]]; then
+    stop
+    echo "Flask Stopped"
+fi
