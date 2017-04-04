125cbc05c690723085c29f51e8356b5acad6a5af
sbrumley <sbrumley@wireless-town.com>
2017-04-04 11:03:39 -0400

    Created URL Test case function

--- tests/urltests.sh
+++ tests/urltests.sh
@@ -1,8 +1,67 @@
 #!/usr/bin/env bash
+set -e
 
-wget 'http://127.0.0.1:5000/tie/getfile/?sha1=D4186881780D48BF55D4D59171B115634E3C7BA6&json=true' -O - >> hi.json
-wget 'http://127.0.0.1:5000/tie/getfile/?sha256=2859635FEBCC5C38470828DAAECFF49179716ADDFC5AD9FADEB89722842B381A&json=true' -O - >> hi.json
-wget 'http://127.0.0.1:5000/tie/getfile/?md5=836E935C5539ED23FAD863CB823C0A8A&json=true' -O - >> hi.json
+source /vagrant/tests/vars.sh
 
+### Make sure Web Service Reponds with HTTP Code of 200
+function test_http_code {
+    TEST_NAME="HTTP CODE 200"
+    echo "#### TEST: ${TEST_NAME} ####"
+    echo "     Testing ${TEST_URL}/about ####"
+    HTTP_CODE=$(sudo wget --spider -O - -S "${TEST_URL}/about" 2>&1 | grep "HTTP/" | awk '{print $2}')
+    if [[ ${HTTP_CODE} == "200" ]]; then
+        echo "TEST SUCCESS: ${TEST_NAME}"
+    else
+        echo "TEST FAILED: ${TEST_NAME}"
+        exit 1
+    fi
+}
 
-wget --spider -O - -S "http://127.0.0.1:5000/about" 2>&1 | grep "HTTP/" | awk '{print $2}'
+function test_sha1 {
+    TEST_NAME="SHA1 File Hash"
+    echo "#### TEST: ${TEST_NAME} ####"
+    echo "     Testing ${TEST_URL}/tie/getfile/?sha1=${SHA1_TEST}&json=true ####"
+    WEB_CONTENT=$(sudo wget -O - "${TEST_URL}/tie/getfile/?sha1=${SHA1_TEST}&json=true" 2>&1)
+    if [[ ${WEB_CONTENT} == *"error"* ]]; then
+        echo "TEST FAILED: ${TEST_NAME}"
+        echo $WEB_CONTENT
+        exit 1
+    else
+        echo "TEST SUCCESS: ${TEST_NAME}"
+    fi
+}
+
+function test_sha256 {
+    TEST_NAME="SHA256 File Hash"
+    echo "#### TEST: ${TEST_NAME} ####"
+    echo "     Testing ${TEST_URL}/tie/getfile/?sha256=${SHA256_TEST}&json=true ####"
+    WEB_CONTENT=$(sudo wget -O - "${TEST_URL}/tie/getfile/?sha256=${SHA256_TEST}&json=true" 2>&1)
+    if [[ ${WEB_CONTENT} == *"error"* ]]; then
+        echo "TEST FAILED: ${TEST_NAME}"
+        echo $WEB_CONTENT
+        exit 1
+    else
+        echo "TEST SUCCESS: ${TEST_NAME}"
+    fi
+}
+
+function test_md5 {
+    TEST_NAME="MD5 File Hash"
+    echo "#### TEST: ${TEST_NAME} ####"
+    echo "     Testing ${TEST_URL}/tie/getfile/?md5=${MD5_TEST}&json=true ####"
+    WEB_CONTENT=$(sudo wget -O - "${TEST_URL}/tie/getfile/?md5=${MD5_TEST}&json=true" 2>&1)
+    if [[ ${WEB_CONTENT} == *"error"* ]]; then
+        echo "TEST FAILED: ${TEST_NAME}"
+        echo $WEB_CONTENT
+        exit 1
+    else
+        echo "TEST SUCCESS: ${TEST_NAME}"
+    fi
+}
+
+### Begin Testing ###
+
+test_http_code
+test_sha1
+test_sha256
+echo "All Test Successful"
