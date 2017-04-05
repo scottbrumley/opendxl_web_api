1d828541d7b9788b69a707ef328fa30e81501a4c
sbrumley <sbrumley@wireless-town.com>
2017-04-05 16:03:52 -0400

    for future expansion

--- vclean.sh
+++ vclean.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 vagrant destroy -f web
-vagrant destroy -f cuckoo
-vagrant destroy -f cuckoo1
+#vagrant destroy -f cuckoo
+#vagrant destroy -f cuckoo1
 rm -rf opendxl-client-python
 rm -rf opendxl-tie-client-python
 rm -rf cuckoo
