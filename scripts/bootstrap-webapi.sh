a07a60610183d48a7c1fb3f075f64ba43124eeea
sbrumley <sbrumley@wireless-town.com>
2017-04-05 15:55:17 -0400

    Update port info for environment

--- scripts/bootstrap-webapi.sh
+++ scripts/bootstrap-webapi.sh
@@ -76,4 +76,5 @@ sudo touch /vagrant/dxlclient.config
 ## Setup Flask
 ## Use flask run --host=0.0.0.0 to start Flask
 sudo pip install Flask
+sudo export FLASK_PORT=5000   ## Configure Flask Port
 sudo echo 'export FLASK_APP=/vagrant/opendxl_web_api.py' >> /etc/bash.bashrc
