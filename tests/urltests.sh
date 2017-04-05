c95da6879152f3dcb02faf4f9c74a6786b447e96
sbrumley <sbrumley@wireless-town.com>
2017-04-05 09:13:54 -0400

    Adding token to tests

--- tests/urltests.sh
+++ tests/urltests.sh
@@ -8,7 +8,7 @@ function test_http_code {
     TEST_NAME="HTTP CODE 200"
     echo "#### TEST: ${TEST_NAME} ####"
     echo "     Testing ${TEST_URL}/about ####"
-    HTTP_CODE=$(sudo wget --spider -O - -S "${TEST_URL}/about" 2>&1 | grep "HTTP/" | awk '{print $2}')
+    HTTP_CODE=$(sudo wget --spider -O - -S "${TEST_URL}/about&token=${FLASK_TOKEN}" 2>&1 | grep "HTTP/" | awk '{print $2}')
     if [[ ${HTTP_CODE} == "200" ]]; then
         echo "TEST SUCCESS: ${TEST_NAME}"
     else
@@ -22,7 +22,7 @@ function test_sha1 {
     TEST_NAME="SHA1 File Hash"
     echo "#### TEST: ${TEST_NAME} ####"
     echo "     Testing ${TEST_URL}/tie/getfile/?sha1=${SHA1_TEST}&json=true ####"
-    WEB_CONTENT=$(sudo wget -O - "${TEST_URL}/tie/getfile/?sha1=${SHA1_TEST}&json=true" 2>&1)
+    WEB_CONTENT=$(sudo wget -O - "${TEST_URL}/tie/getfile/?sha1=${SHA1_TEST}&token=${FLASK_TOKEN}&json=true" 2>&1)
     if [[ ${WEB_CONTENT} == *"error"* ]]; then
         echo ""
         echo "TEST FAILED: ${TEST_NAME}"
@@ -37,7 +37,7 @@ function test_sha256 {
     TEST_NAME="SHA256 File Hash"
     echo "#### TEST: ${TEST_NAME} ####"
     echo "     Testing ${TEST_URL}/tie/getfile/?sha256=${SHA256_TEST}&json=true ####"
-    WEB_CONTENT=$(sudo wget -O - "${TEST_URL}/tie/getfile/?sha256=${SHA256_TEST}&json=true" 2>&1)
+    WEB_CONTENT=$(sudo wget -O - "${TEST_URL}/tie/getfile/?sha256=${SHA256_TEST}&token=${FLASK_TOKEN}&json=true" 2>&1)
     if [[ ${WEB_CONTENT} == *"error"* ]]; then
         echo ""
         echo "TEST FAILED: ${TEST_NAME}"
@@ -52,7 +52,7 @@ function test_md5 {
     TEST_NAME="MD5 File Hash"
     echo "#### TEST: ${TEST_NAME} ####"
     echo "     Testing ${TEST_URL}/tie/getfile/?md5=${MD5_TEST}&json=true ####"
-    WEB_CONTENT=$(sudo wget -O - "${TEST_URL}/tie/getfile/?md5=${MD5_TEST}&json=true" 2>&1)
+    WEB_CONTENT=$(sudo wget -O - "${TEST_URL}/tie/getfile/?md5=${MD5_TEST}&token=${FLASK_TOKEN}&json=true" 2>&1)
     if [[ ${WEB_CONTENT} == *"error"* ]]; then
         echo ""
         echo "TEST FAILED: ${TEST_NAME}"
