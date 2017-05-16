4cfefd898d98a3f093630b53cd4175b45bbab0ee
sbrumley <sbrumley@wireless-town.com>
2017-05-16 08:46:19 -0400

    detecting root_dir

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,44 +1,50 @@
 #!/bin/bash
 
+if [[ -d "/vagrant" ]]; then
+    ROOT_DIR="/vagrant/"
+else
+    ROOT_DIR="$(pwd)/"
+fi
+
 function installOpenDXLClient {
     ### Install Open DXL Client
     echo "Installing Open DXL Client"
-    cd /vagrant
+    cd /${ROOT_DIR}
     sudo git clone https://github.com/opendxl/opendxl-client-python.git
-    cd /vagrant/opendxl-client-python
+    cd /${ROOT_DIR}/opendxl-client-python
     sudo python setup.py install
 }
 
 function installOpenDXLTIEClient {
     ### Install Open DXL TIE Client
     echo "Installing Open DXL TIE Client"
-    cd /vagrant
+    cd /${ROOT_DIR}
     sudo git clone https://github.com/opendxl/opendxl-tie-client-python.git
-    cd /vagrant/opendxl-tie-client-python
+    cd /${ROOT_DIR}/opendxl-tie-client-python
     sudo python setup.py install
 }
 
 function installePOClient {
     ### Install Open DXL TIE Client
     echo "Installing Open DXL ePO Client"
-    cd /vagrant
+    cd /${ROOT_DIR}
     sudo git clone https://github.com/opendxl/opendxl-epo-client-python.git
-    cd /vagrant/opendxl-epo-client-python
+    cd /${ROOT_DIR}/opendxl-epo-client-python
     sudo python setup.py install
 }
 
 function installMARClient {
     ### Install Open DXL MAR Client
     echo "Installing Open DXL TIE Client"
-    cd /vagrant
+    cd /${ROOT_DIR}
     sudo git clone https://github.com/opendxl/opendxl-mar-client-python.git
-    cd /vagrant/opendxl-mar-client-python
+    cd /${ROOT_DIR}/opendxl-mar-client-python
     sudo python setup.py install
 }
 
 function buildCertsFolders {
     ### Create Directories
-    sudo mkdir -p /vagrant/brokercerts
-    sudo mkdir -p /vagrant/certs
-    sudo touch /vagrant/dxlclient.config
+    sudo mkdir -p /${ROOT_DIR}/brokercerts
+    sudo mkdir -p /${ROOT_DIR}/certs
+    sudo touch /${ROOT_DIR}/dxlclient.config
 }
