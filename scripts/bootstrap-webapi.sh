80e6692464759fc43015eaf03d92e935e56bc156
sbrumley <sbrumley@wireless-town.com>
2017-05-16 08:46:36 -0400

    detecting root_dir

--- scripts/bootstrap-webapi.sh
+++ scripts/bootstrap-webapi.sh
@@ -8,4 +8,4 @@ installePOClient
 buildCertsFolders
 
 ## Setup Flask Environment
-sudo echo 'export FLASK_APP=/vagrant/opendxl_web_api.py' >> /etc/bash.bashrc
+sudo echo 'export FLASK_APP=/${ROOT_DIR}/opendxl_web_api.py' >> /etc/bash.bashrc
