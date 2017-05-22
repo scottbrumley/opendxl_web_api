aeb4510263a3469fb643aa38e7e44d71b5fff1d5
sbrumley <sbrumley@wireless-town.com>
2017-05-22 10:20:08 -0400

    For automated docker builds the normal Dockerfile name needs to be used.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,12 +4,12 @@ source scripts/vars.sh
 
 if [ "${1}" == "push" ]; then
     echo "Building and Pushing Container ${LOCAL_BULD} to Docker Hub"
-    docker build -t ${LOCAL_BUILD} -f Dockerfile.build .
+    docker build -t ${LOCAL_BUILD} -f Dockerfile .
     docker login
     echo "Tagging Container ${LOCAL_BUILD} to ${REMOTE_BUILD} for Shipping"
     docker tag ${LOCAL_BUILD} ${REMOTE_BUILD}
     docker push ${REMOTE_BUILD}
 else
    echo "Building Local Image ${LOCAL_BUILD}"
-   docker build -t ${LOCAL_BUILD} -f Dockerfile.build .
+   docker build -t ${LOCAL_BUILD} -f Dockerfile .
 fi
