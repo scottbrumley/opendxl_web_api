a2efb6c927ad156c3b5fcbec26e5f07bd372d8dd
sbrumley <sbrumley@wireless-town.com>
2017-08-17 12:52:43 -0400

    Bootstrap using pip requirements

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,14 +5,19 @@ source scripts/common.sh
 
 echo "Checking Sudo ... "
 installSudo
-echo "Installing Flask ... "
-installFlask
-echo "Installing OpenDXL TIE"
-installOpenDXLTIEClient
-installFlaskSockets
+
+sudo pip install -r requirements.txt
+
+#echo "Installing Flask ... "
+#installFlask
+#echo "Installing OpenDXL TIE"
+#installOpenDXLTIEClient
+#installFlaskSockets
+
 #installMARClient
 #installePOClient
 #buildCertsFolders
+
 echo "Cleanup"
 cleanUp
 
